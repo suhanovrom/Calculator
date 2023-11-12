c95c4290a4f7847859a3001fb4f9978ce517779b
suhanovrom <suhanovrom@gmail.com>
2023-11-12 10:02:38 +0200

    optimize

--- main.js
+++ main.js
@@ -1,91 +1,62 @@
 let result = document.querySelector("h1");
-let one = document.querySelector(".one");
-let two = document.querySelector(".two");
-let three = document.querySelector(".three");
-let four = document.querySelector(".four");
-let five = document.querySelector(".five");
-let six = document.querySelector(".six");
-let seven = document.querySelector(".seven");
-let eight = document.querySelector(".eight");
-let nine = document.querySelector(".nine");
-let zero = document.querySelector(".zero");
-let minus = document.querySelector(".minus");
-let plus = document.querySelector(".plus");
-let divide = document.querySelector(".divide");
-let equals = document.querySelector(".equals");
-let multi = document.querySelector(".multi");
 let ac = document.querySelector(".ac");
-let bracket1 = document.querySelector(".bracket1");
-let bracket2 = document.querySelector(".bracket2");
-let dot = document.querySelector(".dot");
 let operators = document.querySelectorAll(".operators");
-let pi = document.querySelector(".pi");
-let sqrt = document.querySelector(".sqrt");
-bracket1.textContent = "(";
-bracket2.textContent = ")";
-bracket1.style.color = "white";
-bracket2.style.color = "white";
 let number = document.querySelectorAll(".number");
 for (let value of number){
     value.addEventListener("click", function() {
         result.textContent  += value.textContent;
     });
 }
-
-plus.addEventListener("click", function() {
-    result.textContent += "+";
-});
-minus.addEventListener("click", function() {
-    result.textContent += "-";
-});
-multi.addEventListener("click", function() {
-    result.textContent += "*";
-});
-divide.addEventListener("click", function() {
-    result.textContent += "/";
-});
-equals.addEventListener("click", function() {
-    if(((result.textContent.includes("/0") == true) && (result.textContent.includes("/0.") == false)) || (result.textContent == "0/0")){
-        alert("На 0 делить нельзя");
-        result.textContent = "";
-    }else if((eval(result.textContent) ==  Infinity) || (eval(result.textContent) == -Infinity)){
-        alert("Вы немного переборщили");
-        result.textContent = "";
-    }else if(result.textContent == "3.12"){
-        result.textContent = "Happy Birthday Dad";
-    }else{
-        result.textContent = eval(result.textContent);
+for(let value of operators){
+    switch(value.textContent){
+        case "÷":
+            value.addEventListener("click", function() {
+                result.textContent += "/";
+            });
+            break;
+        case "×":
+            value.addEventListener("click", function() {
+                result.textContent += "*";
+            });
+            break;
+        case "√":
+            value.addEventListener("click", function() {
+                if (eval(result.textContent) < 0){
+                    alert("нельзя вычеслить корень");
+                    result.textContent = "";
+                }else{
+                    let a = Math.sqrt(eval(result.textContent));
+                    result.textContent = a.toFixed(2);
+                }
+            });
+            break;
+        case "Π":
+            value.addEventListener("click", function() {
+                result.textContent += Math.PI.toFixed(2);
+            });
+            break;
+        case "=":
+            value.addEventListener("click", function() {
+                if(((result.textContent.includes("/0") == true) && (result.textContent.includes("/0.") == false)) || (result.textContent == "0/0")){
+                    alert("На 0 делить нельзя");
+                    result.textContent = "";
+                }else if((eval(result.textContent) ==  Infinity) || (eval(result.textContent) == -Infinity)){
+                    alert("Вы немного переборщили");
+                    result.textContent = "";
+                }else if(result.textContent == "3.12"){
+                    result.textContent = "Happy Birthday Dad";
+                }else{
+                    result.textContent = eval(result.textContent);
+                } 
+            });
+            break;
+        default:
+            value.addEventListener("click", function() {
+                result.textContent  += value.textContent;
+            });
+            break;
     }
-
-
-   
-    
-    
-});
-pi.addEventListener("click", function() {
-    result.textContent += Math.PI.toFixed(2);
-});
-ac.addEventListener("click", function() {
-    result.textContent = "";
-});
-dot.addEventListener("click", function() {
-    result.textContent += ".";
-});
-bracket1.addEventListener("click", function() {
-    result.textContent += "(";
-});
-bracket2.addEventListener("click", function() {
-    result.textContent += ")";
-});
-sqrt.addEventListener("click", function() {
-    if (eval(result.textContent) < 0){
-        alert("нельзя вычеслить корень");
-        result.textContent = "";
-    }else{
-        let a = Math.sqrt(eval(result.textContent));
-        result.textContent = a.toFixed(2);
-    }
-});
+}
 document.addEventListener('keydown', function(e) {
     for(let value of number){
         if(e.key == value.textContent){
@@ -116,16 +87,7 @@ document.addEventListener('keydown', function(e) {
         result.textContent += Math.PI.toFixed(2);
     }
   });
-//   let div = document.querySelector(".background");
-//   let listener = function(e) {
-//     div.style.left = e.pageX - 50 + "px";
-//     div.style.top = e.pageY - 50 + "px";
-//   };
-  
-//   div.addEventListener('mousedown', e => {
-//       document.addEventListener('mousemove', listener);
-//   });
-  
-//   div.addEventListener('mouseup', e => {
-//       document.removeEventListener('mousemove', listener);
-//   });
+
+ac.addEventListener("click", function() {
+    result.textContent = "";
+});
